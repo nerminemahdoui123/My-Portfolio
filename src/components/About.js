43f17f7574592b4d0c26f061c01706aa38449a67
Votre Nom <mahdoui.nermine@gmail.com>
2024-11-22 23:43:05 +0100

    about

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const About = () => {
+  return (
+    <section className="about-section">
+      <h2>À propos de moi</h2>
+      <p>
+        Passionnée par le développement web et mobile, je crée des expériences utilisateur ergonomiques et performantes.
+      </p>
+    </section>
+  );
+};
+
+export default About;
